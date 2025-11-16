049dc513d566bc7c91eb80d178e3788510fa0d56
Emilly Pinheiro <esilvapinheiro2006@gmail.com>
2025-11-16 00:55:59 -0300

    initial commit

---
+++ index.py
@@ -0,0 +1,18 @@
+print('====Boas vindas a sua agenda digital!====')
+
+days = {
+    "segunda": [],
+    "terça": [],
+    "quarta": [],
+    "quinta": [],
+    "sexta": [],
+    "sábado": [],
+    "domingo": []
+}
+
+
+day = input("Digite o dia da semana que deseja adicionar uma tarefa: ")
+
+
+
+def add_task(day, task):
